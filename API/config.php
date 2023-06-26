0078355a3f90e76e2402282b2dc70b1f3f472a58
R8nDr0p <rainchrstn@gmail.com>
2023-06-27 07:08:51 +0800

    added the necessary html files

--- API/config.php
+++ API/config.php
@@ -8,6 +8,4 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
     die("Error Connecting to " . $conn->connect_error);
-} else {
-    echo "connection established successfully";
 }
--- API/register.php
+++ API/register.php
@@ -1,6 +1,6 @@
 <?php
-
-require_once 'config.php';
+session_start();
+include 'config.php';
 
 class RegisterEndpoint
 {
